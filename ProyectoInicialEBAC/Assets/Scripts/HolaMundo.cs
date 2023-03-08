d007f3b268551bf24dce41cc38b1830f790fb467
jrzam92 <jr_zam92@outlook.com>
2023-03-07 21:43:22 -0600

    Creacion de hola mundo
    Se realiza cambio de nombre de escena, se crea tambien la clase de hola mundo

---
+++ ProyectoInicialEBAC/Assets/Scripts/HolaMundo.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Formas de imprimir en consola 
+        print("Funciona para imprimir en consola");
+        //Log de informacion
+        Debug.Log("Hola Mundo Ebac!!!!");
+        //Log de error
+        Debug.LogError("Algo salio mal");
+        //Log de warning
+        Debug.LogWarning("Algo salio medianamente mal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
